38669fc0fcb012fedf9c0634c0a4e7d509007f30
Mathan-cyber2 <24149030@drngpit.ac.in>
2026-02-04 15:33:07 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,15 @@
+// Last updated: 2/4/2026, 3:33:06 PM
+class Solution {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num - 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
