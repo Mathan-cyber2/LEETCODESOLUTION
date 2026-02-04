8b53f2768356661518d3993c7232f9d332317ef3
Mathan-cyber2 <24149030@drngpit.ac.in>
2026-02-04 15:33:14 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,9 @@
+// Last updated: 2/4/2026, 3:33:13 PM
+public class Solution {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
