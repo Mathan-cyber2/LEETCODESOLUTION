654dde1c9898671454916bcf5023847dc474bd6b
Mathan-cyber2 <24149030@drngpit.ac.in>
2026-02-04 15:33:04 +0530

    Create 1635-NumberOfGoodPairs

---
+++ 1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 3:33:03 PM
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
